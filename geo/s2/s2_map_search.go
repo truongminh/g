6a4abc9794a0dab3ef08952389f95c75e21bb2a1
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2016-09-22 18:39:50 +0700

    search nearest

--- geo/s2/s2_map_search.go
+++ geo/s2/s2_map_search.go
@@ -1,6 +1,8 @@
 package s2
 
 import (
+	"sort"
+
 	"github.com/golang/geo/s2"
 )
 
@@ -23,4 +25,24 @@ func (m *Map) ForEachWithin(lat float32, lng float32, km float32, f func(IEntry,
 			}
 		})
 	}
+}
+
+type view struct {
+	IEntry
+	Km float32
+}
+
+type viewByKm []view
+
+func (a viewByKm) Len() int           { return len(a) }
+func (a viewByKm) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a viewByKm) Less(i, j int) bool { return a[i].Km < a[j].Km }
+
+func (m *Map) NearestWithin(lat float32, lng float32, km float32, limit int32) []view {
+	var res = viewByKm(make([]view, 0))
+	m.ForEachWithin(lat, lng, km, func(e IEntry, d float32) {
+		res = append(res, view{IEntry: e, Km: d})
+	})
+	sort.Sort(res)
+	return res
 }
